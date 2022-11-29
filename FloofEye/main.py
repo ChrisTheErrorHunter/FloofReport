093151d79cf7ec691d25cb914e09d3cf72c17eca
Krzysztof Mulawa <78088958+ChrisTheErrorHunter@users.noreply.github.com>
2022-11-29 19:14:21 +0100

    Added proper time calculation

--- FloofEye/main.py
+++ FloofEye/main.py
@@ -11,12 +11,13 @@ from datetime import datetime, timedelta
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    cap = cv2.VideoCapture("C:/Users/Krzysztof/Videos/04-39-29.mp4")
+    cap = cv2.VideoCapture("C:/Users/mrkri/Videos/v1123/04-39-29.mp4")
     frame_num = 0
     fps = cap.get(cv2.CAP_PROP_FPS)
-    raw_time = '2022-11-23 04:34:56.56110'
+    raw_time = '2022-11-23 04:34:56.5611'
     FDOB = datetime.strptime(raw_time, '%Y-%m-%d %H:%M:%S.%f')
     print(fps, "FPS")
+    milesecound_per_frame = 1000 / fps
 
     object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
 
@@ -86,10 +87,10 @@ if __name__ == '__main__':
             if 40 < area < 5000:
                 tmp = (where_is_outline(cnt[0][0][1], cnt[0][0][0]))
                 if 0 != tmp != currentLocation:
-                    time_to_insert = FDOB + timedelta(milliseconds=400)
+                    time_to_insert = FDOB + timedelta(milliseconds=milesecound_per_frame) * frame_num
                     currentLocation = tmp
                     cur = conn.cursor()
-                    cur.execute('''INSERT INTO visualevents (registrationtime, cageid, hamsterid, areaid) VALUES (now() + interval '1 hour', 1, 1, %d);''' %(currentLocation))
+                    cur.execute('''INSERT INTO visualevents (registrationtime, cageid, hamsterid, areaid) VALUES ('%s', 1, 1, %d);''' %(time_to_insert, currentLocation))
                     conn.commit()
                     print('''Saved %d area into db''' %(currentLocation))
                     cur.close()
