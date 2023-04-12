6c5febd0c3e194a3de7b769b59dc17cf8f29b6ae
Krzysztof Mulawa <mrkristoso@gmail.com>
2023-04-12 20:31:33 +0200

    Implemented configuration file

--- FloofReport/FloofReport/Models/HamsterBookContext.cs
+++ FloofReport/FloofReport/Models/HamsterBookContext.cs
@@ -2,6 +2,7 @@
 #nullable disable
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -27,8 +28,7 @@ namespace FloofReport.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Password=Flafik,456;Username=postgres;Database=HamsterBook;Host=theoldesthouse.ddns.net");
+                optionsBuilder.UseNpgsql(ConfigurationManager.AppSettings["ConnectionString"]);
             }
         }
 
